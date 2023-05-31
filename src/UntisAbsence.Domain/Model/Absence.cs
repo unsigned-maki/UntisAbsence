4a7a1fb0ee7f74cd7d7ad08947db601eac4d162a
Niclas Nell <nel20326@spengergasse.at>
2023-05-31 11:14:06 +0200

    feat(absence model): add attributes to model

--- src/UntisAbsence.Domain/Model/Absence.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UntisAbsence.Domain.Model;
-
-public sealed record Absence
-{
-    public int AbsenceId { get; set; }
-    public DateOnly AbsenceDate { get; set; }
-}
---
+++ src/UntisAbsence.Domain/Models/Absence.cs
@@ -0,0 +1,18 @@
+﻿using UntisAbsence.Domain.Enums;
+
+namespace UntisAbsence.Domain.Models;
+
+public sealed record Absence
+{
+    public int AbsenceId { get; init; }
+    
+    public required DateOnly AbsenceDate { get; set; }
+    
+    public required WeekDay Day { get; set; }
+    
+    public required TimeOnly Time { get; set; }
+    
+    public int AbsenceHours { get; set; }
+    
+    public bool Late { get; set; }
+}
